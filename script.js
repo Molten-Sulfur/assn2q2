99779e0f51dbc67a5313422afa665c8d37547ffe
Molten-Sulfur <97858539+Molten-Sulfur@users.noreply.github.com>
2022-01-27 13:57:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
 
-// Use jQuery to grab Greg's SF crime data and add it to the map
-jQuery.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/f94237ceb15208e985207e7f3d48e292d330a6fa/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-    L.geoJson(data).addTo(map);});
+// Define an alternate marker icon
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+// Use jQuery to grab Greg's SF crime data and add it to the map using a new point icon
+jQuery.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/f94237ceb15208e985207e7f3d48e292d330a6fa/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data) {
+  var crimeIcon = L.icon({
+    iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Basic_red_dot.png',
+    iconSize: [10,10]
+  });
+  L.geoJson(data,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: crimeIcon});
+    }
+  }  ).addTo(map);
+});
